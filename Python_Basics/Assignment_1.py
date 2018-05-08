d0683ad228cd664989b5969c639e5f74b62566b0
Santosh Konchada <konchada@csu.fullerton.edu>
2018-05-07 20:35:45 -0700

    Data types in python

---
+++ Python_Basics/Assignment_1.py
@@ -0,0 +1,82 @@
+
+# coding: utf-8
+
+# ## Data Types
+
+# ### int
+
+# In[12]:
+
+my_int_var = 100
+
+
+# In[13]:
+
+print(my_int_var)
+
+
+# In[14]:
+
+my_int_var_2 = 10
+
+
+# In[15]:
+
+print(type(my_int_var_2))
+
+
+# ### float
+
+# In[16]:
+
+my_float_var = 9.9
+
+
+# In[17]:
+
+print(my_float_var)
+
+
+# In[18]:
+
+print(type(my_float_var))
+
+
+# ### string
+
+# In[19]:
+
+my_string = "strings in python"
+
+
+# In[20]:
+
+print(my_string)
+
+
+# In[21]:
+
+print(type(my_string))
+
+
+# ### Boolean
+
+# In[11]:
+
+a = 10
+
+
+# In[22]:
+
+b = 5
+
+
+# In[25]:
+
+c = (a >= b)
+
+
+# In[26]:
+
+print(c)
+
